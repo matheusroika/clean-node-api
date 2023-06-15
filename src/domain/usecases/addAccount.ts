ce3859c89a7361f9bc6b4cd568973079738957d6
Matheus Roika <matheusroika@hotmail.com>
2023-06-15 04:03:43 -0300

    feat: ensure SignUpController calls addAccount with correct values

---
+++ src/domain/usecases/addAccount.ts
@@ -0,0 +1,11 @@
+import type { Account } from '../models/account'
+
+export interface AccountValues {
+  name: string
+  email: string
+  password: string
+}
+
+export interface AddAccount {
+  add: (account: AccountValues) => Account
+}
