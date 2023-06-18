466d0c25ae176976cde690c90873792b645d8c98
Matheus Roika <matheusroika@hotmail.com>
2023-06-18 18:44:25 -0300

    feat: ensure AccountMongoRepository throws if accountCollection.insertOne or accountCollection.findOne throws

--- src/infra/db/mongodb/accountRepository/account.ts
+++ src/infra/db/mongodb/accountRepository/account.ts
@@ -4,10 +4,11 @@ import type { AccountValues } from '../../../../domain/usecases/addAccount'
 import { MongoHelper } from '../helpers/mongoHelper'
 
 export class AccountMongoRepository implements AddAccountRepository {
+  readonly accountCollection = MongoHelper.getCollection('accounts')
+
   async add (account: AccountValues): Promise<Account> {
-    const accountCollection = MongoHelper.getCollection('accounts')
-    const document = await accountCollection.insertOne(account)
-    const newAccount = await accountCollection.findOne({ _id: document.insertedId })
+    const document = await this.accountCollection.insertOne(account)
+    const newAccount = await this.accountCollection.findOne({ _id: document.insertedId })
     if (!newAccount) throw new Error()
     return MongoHelper.map(newAccount)
   }
