d19e0f51a2cf63709c0db2fcf9589cf9397c4be0
Matheus Roika <matheusroika@hotmail.com>
2023-06-18 19:52:19 -0300

    refactor: group middlewares exports in a single file

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,7 +1,5 @@
 import type { Express } from 'express'
-import { bodyParser } from '../middlewares/bodyParser'
-import { cors } from '../middlewares/cors'
-import { contentType } from '../middlewares/contentType'
+import { bodyParser, contentType, cors } from '../middlewares'
 
 export default (app: Express): void => {
   app.use(bodyParser)
---
+++ src/main/middlewares/index.ts
@@ -0,0 +1,4 @@
+/* istanbul ignore file */
+export * from './bodyParser'
+export * from './cors'
+export * from './contentType'
