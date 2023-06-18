7fbdbab19f05f5d9002ba3d55646404d79576383
Matheus Roika <matheusroika@hotmail.com>
2023-06-18 17:13:55 -0300

    test: ensure AccountMongoRepository returns an account on success

---
+++ src/infra/db/mongodb/accountRepository/account.spec.ts
@@ -0,0 +1,31 @@
+import { MongoHelper } from '../helpers/mongoHelper'
+import { AccountMongoRepository } from './account'
+
+const makeSut = (): AccountMongoRepository => {
+  const sut = new AccountMongoRepository()
+  return sut
+}
+
+describe('Account MongoDB Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL as string)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  test('Should return an account on success', async () => {
+    const sut = makeSut()
+    const account = await sut.add({
+      name: 'Any Name',
+      email: 'any@email.com',
+      password: 'any_password'
+    })
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe('Any Name')
+    expect(account.email).toBe('any@email.com')
+    expect(account.password).toBeTruthy()
+  })
+})
