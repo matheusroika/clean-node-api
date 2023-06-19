60fc8dacb0e828fd898f5a023312870d9e763efb
Matheus Roika <matheusroika@hotmail.com>
2023-06-19 01:25:49 -0300

    feat: ensure AccountMongoRepository throws if newAccount can't be found on DB

--- src/infra/db/mongodb/accountRepository/account.ts
+++ src/infra/db/mongodb/accountRepository/account.ts
@@ -1,11 +1,16 @@
+import type { Collection, Document } from 'mongodb'
 import type { AddAccountRepository } from '../../../../data/protocols/addAccountRepository'
 import type { Account } from '../../../../domain/models/account'
 import type { AccountValues } from '../../../../domain/usecases/addAccount'
 import { MongoHelper } from '../helpers/mongoHelper'
 
 export class AccountMongoRepository implements AddAccountRepository {
+  getAccountCollection (): Collection<Document> {
+    return MongoHelper.getCollection('accounts')
+  }
+
   async add (account: AccountValues): Promise<Account> {
-    const accountCollection = MongoHelper.getCollection('accounts')
+    const accountCollection = this.getAccountCollection()
     const document = await accountCollection.insertOne(account)
     const newAccount = await accountCollection.findOne({ _id: document.insertedId })
     if (!newAccount) throw new Error()
