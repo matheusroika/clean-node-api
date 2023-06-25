7ee35ca5da03fd679b46a2edd7b4554d1bfae7c9
Matheus Roika <matheusroika@hotmail.com>
2023-06-24 23:09:57 -0300

    test: ensure JwtAdapter returns a token on jwt.sign success

--- src/infra/cryptography/jwtAdapter/jwtAdapter.spec.ts
+++ src/infra/cryptography/jwtAdapter/jwtAdapter.spec.ts
@@ -13,11 +13,23 @@ const makeSut = (): Sut => {
   }
 }
 
+jest.mock('jsonwebtoken', () => ({
+  sign (): string {
+    return 'any_token'
+  }
+}))
+
 describe('JSON Web Token Adapter', () => {
   test('Should call jwt.sign with correct values', async () => {
     const { sut } = makeSut()
     const signSpy = jest.spyOn(jwt, 'sign')
     await sut.encrypt('any_id')
     expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, secret)
+  })
+
+  test('Should return a token on jwt.sign success', async () => {
+    const { sut } = makeSut()
+    const accessToken = await sut.encrypt('any_id')
+    expect(accessToken).toBe('any_token')
   })
 })
