1e11507e9f849399e6231cce295b1391288c84c6
Matheus Roika <matheusroika@hotmail.com>
2023-06-25 00:06:27 -0300

    feat: ensure AccountRepository returns null if Account.loadByEmail fails

--- src/infra/db/mongodb/accountRepository/account.ts
+++ src/infra/db/mongodb/accountRepository/account.ts
@@ -21,7 +21,6 @@ export class AccountMongoRepository implements AddAccountRepository, LoadAccount
   async loadByEmail (email: string): Promise<Account | null> {
     const accountCollection = await this.getAccountCollection()
     const account = await accountCollection.findOne({ email })
-    if (!account) throw new Error()
-    return MongoHelper.map(account)
+    return account ? MongoHelper.map(account) : null
   }
 }
