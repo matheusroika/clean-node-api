3c6a23ef41b041c8aed3422cb82f96ddcdcfba1e
Matheus Roika <matheusroika@hotmail.com>
2023-06-25 17:04:22 -0300

    refactor: change routes to fix deployment build

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -5,7 +5,7 @@ import type { Express } from 'express'
 export default (app: Express): void => {
   const router = Router()
   app.use('/api', router)
-  fg.sync('**/src/main/routes/**/**Routes.ts').map(async file => {
-    (await import(`../../../${file}`)).default(router)
+  fg.sync('../routes/**/**Routes.{ts,js}', { cwd: __dirname }).map(async file => {
+    (await import(file)).default(router)
   })
 }
