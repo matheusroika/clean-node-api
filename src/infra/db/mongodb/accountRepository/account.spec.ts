8f2de4c4220518e9183a00998ec842b161d240cc
Matheus Roika <matheusroika@hotmail.com>
2023-06-25 00:03:41 -0300

    test: ensure AccountRepository returns an account on Account.loadByEmail success

--- src/infra/db/mongodb/accountRepository/account.spec.ts
+++ src/infra/db/mongodb/accountRepository/account.spec.ts
@@ -2,6 +2,7 @@ import { MongoHelper } from '../helpers/mongoHelper'
 import { AccountMongoRepository } from './account'
 import type { Collection } from 'mongodb'
 import type { AccountValues } from '../../../../domain/usecases/addAccount'
+import type { Account } from '../../../../domain/models/account'
 
 interface Sut {
   sut: AccountMongoRepository
@@ -72,5 +73,17 @@ describe('Account MongoDB Repository', () => {
     jest.spyOn(accountCollection, 'findOne').mockResolvedValue(null)
     const promise = sut.add(makeFakeAccountValues())
     await expect(promise).rejects.toThrow()
+  })
+
+  test('Should return an account on Account.loadByEmail success', async () => {
+    const { sut } = await makeSut()
+    const accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.insertOne(makeFakeAccountValues())
+    const account = await sut.loadByEmail('any@email.com') as Account
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe('Any Name')
+    expect(account.email).toBe('any@email.com')
+    expect(account.password).toBe('any_password')
   })
 })
