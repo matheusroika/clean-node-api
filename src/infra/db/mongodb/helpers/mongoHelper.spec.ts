14022de6a08b0479de2fbe4423910ede6157727d
Matheus Roika <matheusroika@hotmail.com>
2023-06-25 06:16:23 -0300

    test: ensure 100% coverage on mongoHelper ensure mongoHelper returns false if reconnect made all tries ensure mongoHelper throws if getCollection throws ensure mongoHelper throws if disconnect throws

--- src/infra/db/mongodb/helpers/mongoHelper.spec.ts
+++ src/infra/db/mongodb/helpers/mongoHelper.spec.ts
@@ -21,4 +21,34 @@ describe('Mongo Helper', () => {
     accountCollection = await sut.getCollection('accounts')
     expect(accountCollection).toBeTruthy()
   })
+
+  describe('Connect Spy Group', () => {
+    let connectSpy: jest.SpyInstance<Promise<void>, [uri: string], any>
+
+    beforeEach(async () => {
+      connectSpy = jest.spyOn(sut, 'connect').mockImplementation(async () => { sut.client = null })
+      await sut.disconnect()
+    })
+
+    afterEach(async () => {
+      connectSpy.mockRestore()
+      await sut.connect(process.env.MONGO_URL as string)
+    })
+    test('Should return false if reconnect made all tries', async () => {
+      const reconnectSpy = jest.spyOn(sut, 'reconnect')
+      const reconnect = await sut.reconnect(process.env.MONGO_URL as string, 3, 100)
+      expect(reconnectSpy).toHaveBeenCalledTimes(3)
+      expect(reconnect).toBe(false)
+    })
+
+    test('Should throw if getCollection throws', async () => {
+      const promise = sut.getCollection('accounts')
+      await expect(promise).rejects.toThrow()
+    }, 50000)
+
+    test('Should throw if disconnect throws', async () => {
+      const promise = sut.disconnect()
+      await expect(promise).rejects.toThrow()
+    })
+  })
 })
