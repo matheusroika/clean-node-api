c08e8b93bc7f3dcf4252fc7ae5c2dbdd8feec99e
Matheus Roika <matheusroika@hotmail.com>
2023-07-02 06:05:36 -0300

    feat: ensure JwtAdapter calls jwt.verify with correct values

--- src/infra/cryptography/JwtAdapter/JwtAdapter.ts
+++ src/infra/cryptography/JwtAdapter/JwtAdapter.ts
@@ -1,8 +1,9 @@
 import jwt from 'jsonwebtoken'
 import type { Secret } from 'jsonwebtoken'
 import type { Encrypter } from '../../../data/protocols/cryptography/Encrypter'
+import type { Decrypter } from '../../../data/protocols/cryptography/Decrypter'
 
-export class JwtAdapter implements Encrypter {
+export class JwtAdapter implements Encrypter, Decrypter {
   constructor (
     private readonly secret: Secret,
     private readonly isKey: boolean
@@ -13,5 +14,10 @@ export class JwtAdapter implements Encrypter {
       ? jwt.sign({ id: value }, this.secret, { algorithm: 'RS256' })
       : jwt.sign({ id: value }, this.secret)
     return accessToken
+  }
+
+  decrypt (value: string): string | null {
+    jwt.verify(value, this.secret)
+    return ''
   }
 }
