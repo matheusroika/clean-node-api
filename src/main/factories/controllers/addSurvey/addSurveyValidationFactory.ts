6e3b1052a19755bb044f7000f39b694f8233e546
Matheus Roika <matheusroika@hotmail.com>
2023-07-02 02:48:37 -0300

    feat: ensure addSurveyValidationFactory calls ValidationComposite with all validations

---
+++ src/main/factories/controllers/addSurvey/addSurveyValidationFactory.ts
@@ -0,0 +1,12 @@
+import { RequiredFieldValidation, ValidationComposite } from '../../../../validation/validators'
+import type { Validation } from '../../../../presentation/protocols/Validation'
+
+export const makeAddSurveyValidation = (): ValidationComposite => {
+  const requiredFields = ['question', 'answers']
+  const validations: Validation[] = []
+  for (const field of requiredFields) {
+    validations.push(new RequiredFieldValidation(field))
+  }
+
+  return new ValidationComposite(validations)
+}
