9960a0e2afae5ca90f97f16dc32aa8e407984581
Matheus Roika <matheusroika@hotmail.com>
2023-07-02 00:05:33 -0300

    test: ensure AddSurveyController returns badRequest 400 if Validation fails

--- src/presentation/controllers/survey/addSurvey/AddSurveyController.spec.ts
+++ src/presentation/controllers/survey/addSurvey/AddSurveyController.spec.ts
@@ -1,5 +1,6 @@
 import type { HttpRequest, Validation } from './AddSurveyControllerProtocols'
 import { AddSurveyController } from './AddSurveyController'
+import { badRequest } from '../../../helpers/http/httpHelper'
 
 const makeValidation = (): Validation => {
   class ValidationStub implements Validation {
@@ -41,5 +42,12 @@ describe('Add Survey Controller', () => {
     const httpRequest = makeFakeRequest()
     await sut.handle(httpRequest)
     expect(validateSpy).toHaveBeenCalledWith(httpRequest.body)
+  })
+
+  test('Should return 400 if Validation fails', async () => {
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new Error())
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(badRequest(new Error()))
   })
 })
