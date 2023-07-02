74b9ad5b85183d66d13bc9938d7915ec41f2ff9c
Matheus Roika <matheusroika@hotmail.com>
2023-07-02 00:23:32 -0300

    feat: ensure AddSurveyController calls AddSurvey with correct values

---
+++ src/domain/useCases/AddSurvey.ts
@@ -0,0 +1,13 @@
+export interface SurveyAnswer {
+  image: string
+  answer: string
+}
+
+export interface AddSurveyValues {
+  question: string
+  answers: SurveyAnswer[]
+}
+
+export interface AddSurvey {
+  add: (data: AddSurveyValues) => Promise<void>
+}
--- src/presentation/controllers/survey/addSurvey/AddSurveyController.ts
+++ src/presentation/controllers/survey/addSurvey/AddSurveyController.ts
@@ -1,14 +1,21 @@
 import { badRequest } from '../../../helpers/http/httpHelper'
-import type { Controller, HttpRequest, HttpResponse, Validation } from './AddSurveyControllerProtocols'
+import type { AddSurvey, Controller, HttpRequest, HttpResponse, Validation } from './AddSurveyControllerProtocols'
 
 export class AddSurveyController implements Controller {
   constructor (
-    private readonly validation: Validation
+    private readonly validation: Validation,
+    private readonly addSurvey: AddSurvey
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const error = this.validation.validate(httpRequest.body)
     if (error) return badRequest(error)
+
+    const { question, answers } = httpRequest.body
+    await this.addSurvey.add({
+      question,
+      answers
+    })
 
     return {
       statusCode: 200,
