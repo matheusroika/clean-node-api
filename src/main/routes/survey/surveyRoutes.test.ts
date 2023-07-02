8669308cac22416a6786c0e520a6a7043edefdbb
Matheus Roika <matheusroika@hotmail.com>
2023-07-02 07:33:51 -0300

    test: ensure surveyRoutes returns 403 on POST /survey from an account with no role with accessToken

--- src/main/routes/survey/surveyRoutes.test.ts
+++ src/main/routes/survey/surveyRoutes.test.ts
@@ -66,5 +66,32 @@ describe('Survey Routes', () => {
         .send(makeFakeSurveyValues())
         .expect(204)
     })
+
+    test('Should return 403 on POST /survey from an account with no role with accessToken', async () => {
+      const accountCollection = await mongoHelper.getCollection('accounts')
+      const document = await accountCollection.insertOne({
+        name: 'Any Name',
+        email: 'any@email.com',
+        password: 'any_password'
+      })
+      const newAccount = await accountCollection.findOne({ _id: document.insertedId })
+      if (!newAccount) return
+      const id = newAccount._id
+      const keyPath = process.env.NODE_ENV === 'deployment' ? './jwtRS256.key' : '**/keys/jwt/jwtRS256.key'
+      const secret = cryptoHelper.getPrivateKeyObject(cryptoHelper.getKeyString(keyPath))
+      const accessToken = jwt.sign({ id }, secret, { algorithm: 'RS256' })
+      await accountCollection.updateOne({
+        _id: id
+      }, {
+        $set: {
+          accessToken
+        }
+      })
+      await request(app)
+        .post('/api/surveys')
+        .set('x-access-token', accessToken)
+        .send(makeFakeSurveyValues())
+        .expect(403)
+    })
   })
 })
