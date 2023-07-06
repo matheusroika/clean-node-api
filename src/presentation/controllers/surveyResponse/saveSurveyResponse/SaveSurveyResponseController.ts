492e2630dc3ef403f877c0b8ca28566934b8efbb
Matheus Roika <matheusroika@hotmail.com>
2023-07-06 06:13:51 -0300

    feat: ensure SaveSurveyResponseController returns 403 if LoadSurveyById returns null

--- src/presentation/controllers/surveyResponse/saveSurveyResponse/SaveSurveyResponseController.ts
+++ src/presentation/controllers/surveyResponse/saveSurveyResponse/SaveSurveyResponseController.ts
@@ -1,4 +1,4 @@
-import { ok } from '@/presentation/middlewares/AuthMiddlewareProtocols'
+import { InvalidParamError, forbidden, ok } from '@/presentation/middlewares/AuthMiddlewareProtocols'
 import type { Controller, HttpRequest, HttpResponse, LoadSurveyById } from './SaveSurveyResponseControllerProtocols'
 
 export class SaveSurveyResponseController implements Controller {
@@ -8,7 +8,8 @@ export class SaveSurveyResponseController implements Controller {
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const surveyId = httpRequest.params.surveyId
-    await this.loadSurveyById.loadById(surveyId)
+    const survey = await this.loadSurveyById.loadById(surveyId)
+    if (!survey) return forbidden(new InvalidParamError('params.surveyId'))
     return ok('')
   }
 }
--- src/presentation/controllers/surveyResponse/saveSurveyResponse/SaveSurveyResponseControllerProtocols.ts
+++ src/presentation/controllers/surveyResponse/saveSurveyResponse/SaveSurveyResponseControllerProtocols.ts
@@ -2,3 +2,4 @@
 export * from '@/presentation/protocols'
 export * from '@/domain/useCases/survey/LoadSurveyById'
 export * from '@/domain/models/Survey'
+export * from '@/presentation/middlewares/AuthMiddlewareProtocols'
