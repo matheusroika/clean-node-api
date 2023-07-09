86f962d30c533e655f99482e6e30dcbbaffa892d
Matheus Roika <matheusroika@hotmail.com>
2023-07-08 23:48:48 -0300

    docs: add loginPath to swagger

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,8 +1,24 @@
+import { loginPath } from './paths'
+import { accountSchema, loginParamsSchema } from './schemas'
+
 export default {
   openapi: '3.0.0',
   info: {
     title: 'Clean Node API',
     description: 'Uma API em Node usando TDD, Clean Architecture, SOLID e Design Patterns, para realizar enquetes',
     version: '1.0.0'
+  },
+  servers: [{
+    url: '/api'
+  }],
+  tags: [{
+    name: 'Authentication'
+  }],
+  paths: {
+    '/login': loginPath
+  },
+  schemas: {
+    account: accountSchema,
+    loginParams: loginParamsSchema
   }
 }
---
+++ src/main/docs/paths/loginPath.ts
@@ -0,0 +1,33 @@
+export const loginPath = {
+  post: {
+    tags: ['Authentication'],
+    summary: 'API para autenticar usuário',
+    requestBody: {
+      content: {
+        'application/json': {
+          schema: {
+            $ref: '#/schemas/loginParams'
+          }
+        }
+      }
+    },
+    responses: {
+      200: {
+        description: 'Sucesso',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/schemas/account'
+            }
+          }
+        }
+      },
+      400: {
+        description: 'Erro: Bad Request'
+      },
+      401: {
+        description: 'Erro: Unauthorized'
+      }
+    }
+  }
+}
