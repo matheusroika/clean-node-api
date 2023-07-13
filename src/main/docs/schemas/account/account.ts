341023708d92971e1afba555d0aa83a362014955
Matheus Roika <matheusroika@hotmail.com>
2023-07-13 16:02:42 -0300

    docs: update account schema

--- src/main/docs/schemas/account/account.ts
+++ src/main/docs/schemas/account/account.ts
@@ -1,6 +1,18 @@
 export const account = {
   type: 'object',
   properties: {
+    id: {
+      type: 'string'
+    },
+    name: {
+      type: 'string'
+    },
+    email: {
+      type: 'string'
+    },
+    password: {
+      type: 'string'
+    },
     accessToken: {
       type: 'string'
     }
