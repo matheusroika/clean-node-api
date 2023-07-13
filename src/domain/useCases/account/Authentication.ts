a9cd9690bea8c49f2e98cde6dc3cc46dbadd9275
Matheus Roika <matheusroika@hotmail.com>
2023-07-13 15:56:19 -0300

    refactor: change return of auth to AuthAccount (Account + accessToken)

--- src/domain/useCases/account/Authentication.ts
+++ src/domain/useCases/account/Authentication.ts
@@ -1,8 +1,14 @@
+import type { Account } from '@/domain/models/Account'
+
 export type AuthParams = {
   email: string
   password: string
 }
 
+export interface AuthAccount extends Account {
+  accessToken: string
+}
+
 export interface Authentication {
-  auth: (authValues: AuthParams) => Promise<string | null>
+  auth: (authValues: AuthParams) => Promise<AuthAccount | null>
 }
