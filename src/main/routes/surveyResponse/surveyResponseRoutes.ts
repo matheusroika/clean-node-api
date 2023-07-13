19a822d2c25103ca94743099c5b5bc792ddc9904
Matheus Roika <matheusroika@hotmail.com>
2023-07-13 10:26:56 -0300

    feat: ensure surveyResponseRoutes returns 403 on GET /surveys/:surveyId/response without accessToken

--- src/main/routes/surveyResponse/surveyResponseRoutes.ts
+++ src/main/routes/surveyResponse/surveyResponseRoutes.ts
@@ -1,8 +1,10 @@
 import { auth } from '@/main/middlewares'
 import { adaptRoute } from '@/main/adapters/express/expressRouteAdapter'
 import { makeSaveSurveyResponseController } from '@/main/factories/controllers/surveyResponse/saveSurveyResponse/saveSurveyResponseControllerFactory'
+import { makeLoadSurveyResponseController } from '@/main/factories/controllers/surveyResponse/loadSurveyResponse/loadSurveyResponseControllerFactory'
 import type { Router } from 'express'
 
 export default (router: Router): void => {
   router.put('/surveys/:surveyId/response', auth(), adaptRoute(makeSaveSurveyResponseController()))
+  router.get('/surveys/:surveyId/response', auth(), adaptRoute(makeLoadSurveyResponseController()))
 }
